d6c1ca284da2bca2fac1164888de1a82b79af307
Mauricio Camacho <mcamacho@bbmg.com>
2011-07-28 18:50:42 -0400

    imagset added

---
+++ js/imagset.js
@@ -0,0 +1,64 @@
+//js with json list of image parameters
+ 
+var imageset = {
+    'fadeTime':1000,
+    'iteration':2000,
+    'pathFolder':'home-assets/',
+    'images': [
+        {
+            'imgfile': 'uja-girl-bg.jpg',
+            'title': 'Girl Writting',
+            'line1': 'Programs like Eden Village connect kids to their Jewish roots while promoting a',
+            'line2': 'sustainable world. Discover how you+UJA can strengthen their Jewish identity.',
+            'link': {
+               'path': 'http://www.bbmg.com',
+               'optCaption': '',
+               'optLink': ''
+                }
+            },
+        {
+            'imgfile': 'uja-oldexercise-bw-bg.jpg',
+            'title': 'All make exercise',
+            'line1': 'View Vimeo Video',
+            'line2': 'MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM',
+            'link': {
+               'path': '<iframe src="http://player.vimeo.com/video/9594822?title=0&amp;byline=0&amp;portrait=0" width="600" height="450" frameborder="0"></iframe>',
+               'optCaption': 'Area for a video caption. Sit amet, consectetur adipiscing eif',
+               'optLink': 'http://www.ujafedny.org'
+                }
+            },
+        {
+            'imgfile': 'uja-girl-bw-bg.jpg',
+            'title': 'Another Girl',
+            'line1': 'Subtitle for Image One,',
+            'line2': 'Subtitle for Image One, Subtitle for Image One, Subtitle for Image One',
+            'link': {
+               'path': 'http://www.ujafedny.org',
+               'optCaption': '',
+               'optLink': ''
+                }
+            },
+        {
+            'imgfile': 'uja-girl-bw-bg.jpg',
+            'title': 'Another Girl',
+            'line1': 'Subtitle for Image One,',
+            'line2': 'Subtitle for Image One, Subtitle for Image One, Subtitle for Image One',
+            'link': {
+               'path': 'http://www.ujafedny.org',
+               'optCaption': '',
+               'optLink': ''
+                }
+            },
+        {
+            'imgfile': 'uja-oldexercise-bg.jpg',
+            'title': 'Its so funny',
+            'line1': 'Subtitle for Image One, Subtitle for Image One, Subtitle for Image One',
+            'line2': 'Subtitle for Image One, Subtitle for Image One, Subtitle for Image One',
+            'link': {
+               'path': 'http://www.ujafedny.org/our-mission/',
+               'optCaption': '',
+               'optLink': ''
+                }
+            }
+    ]
+};
