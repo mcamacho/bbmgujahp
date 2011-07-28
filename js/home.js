c38708fb109b74818f2de2e3402a9dad769754b9
Mauricio Camacho <mcamacho@bbmg.com>
2011-07-28 18:51:37 -0400

    slide plugin dinamic update

--- js/home.js
+++ js/home.js
@@ -1,131 +1,158 @@
 
-var imagset = {"images": [
-        {"uri": "uja-girl-bg.jpg", "title": "Girl Writting", "subtitle": "Subtitle for Image One"},
-        {"uri": "uja-oldexercise-bw-bg.jpg", "title": "All make exercise", "subtitle": "Subtitle for Image two"},
-        {"uri": "uja-girl-bw-bg.jpg", "title": "Another Girl", "subtitle": "Subtitle for Image tree"},
-        {"uri": "uja-oldexercise-bg.jpg", "title": "It's so funny", "subtitle": "Subtitle for Image Four"}
-    ]
-};
-
 var rotate_int;//variable that receives the setinterval return
 var button_index = 0;//variable that receives the index button
-var imgpreload;
-//prelaod images
+
+var jsonAbbrev = imageset.images;//use the json images var
+var totalImages = jsonAbbrev.length;
+var pathFolder = imageset.pathFolder;//use the json imagesPath var
+var iteration = imageset.iteration;//use the json iteration var
+var fadeTime = imageset.fadeTime;//use the json fade var
+
+//preload images
 (function() {
-        var pathAbbrev = imagset.images;
         imgpreload = new Image();
-        var qtt = pathAbbrev.length;
+        qtt = jsonAbbrev.length;
         for(i=0;i<qtt;i++){
-                imgpreload.src = 'home-assets/'+pathAbbrev[i].uri;
+                imgpreload.src = pathFolder + jsonAbbrev[i].imgfile;
         }
 })();
 
 
 //jquery function that initiates when html is loaded
-jQuery(function () {console.log('html ready');
+jQuery(function () {
+        //construct and add the css styles for each background
+        cssInit();
+        
+        //construct nav bar and assign interactivity based on json elements
+        navInit();
+        
+        //initiate the h2, and lines text
+        navText(0);
+        
         //Load the slideshow
-        //rotate_int= setInterval('rotate()', 5000);
+        rotate_int= setInterval('rotate()', iteration);
         
         //set bg-container interactivity
         jQuery('#add-box').click(loadExt).css('cursor', 'pointer');
-        
-        //assign #slide-nav interactivity
-        slideInter();
-        
-        //initiate the h2, h3 texts
-        navText(0);
 });
 
-function navText(index){
-        jQuery('#nav-box h2').text(imagset.images[index].title);
-        jQuery('#nav-box h3').text(imagset.images[index].subtitle);
-}
-
-function slideInter(){
-        jQuery('#slide-nav li[class*=navigator]:eq(0)').click('backward', rotControl).css('cursor', 'pointer');
-        jQuery('#slide-nav li[class*=navigator]:eq(1)').click('foreward', rotControl).css('cursor', 'pointer');
+function cssInit() {
+        //add the styles for the different backgrounds
+        cssString = '<style type="text/css">';
+        for (t in jsonAbbrev){
+                cssString += '#bg-container.home' + t + ' {';
+		cssString += 'background-image: url(' + pathFolder + jsonAbbrev[t].imgfile + ');';
+                cssString += '}';
+        }
+        cssString += '</style>';
+        $(cssString).appendTo('head');
         
-        jQuery('#slide-nav li[class!=navigator]:eq(0)').click({classto:'home1', button:0}, rotation).css('cursor', 'pointer');
-        jQuery('#slide-nav li[class!=navigator]:eq(1)').click({classto:'home2', button:1}, rotation).css('cursor', 'pointer');
-        jQuery('#slide-nav li[class!=navigator]:eq(2)').click({classto:'home3', button:2}, rotation).css('cursor', 'pointer');
-        jQuery('#slide-nav li[class!=navigator]:eq(3)').click({classto:'home4', button:3}, rotation).css('cursor', 'pointer');
+        //add class 'home0' to #bg-container
+        jQuery('#bg-container').addClass('home0');
+}
+function navInit() {
+        //construct the html
+        jQuery('<li class="navigator">&#60</li>')
+                .appendTo('#slide-nav')
+                .click('backward', rotControl)
+                .css('cursor', 'pointer');
+        for (t in jsonAbbrev){
+                jQuery('<li>o</li>')
+                        .appendTo('#slide-nav')
+                        .click({classto:'home' + t, button:t}, rotation)
+                        .css('cursor', 'pointer');
+        }
+        jQuery('<li class="navigator">&#62</li>')
+                .appendTo('#slide-nav')
+                .click('foreward', rotControl)
+                .css('cursor', 'pointer');
+        
+        //add class 'selected' to first li of the nav
+        jQuery('#slide-nav li[class!=navigator]:eq(0)').addClass('selected');
+        
 }
 
-function loadExt() {
-    clearInterval(rotate_int);//stop the rotate loop
-    var current = jQuery('#bg-container').attr('class');//the current bg-container class
-    if (current == 'home1') {
-        jQuery.colorbox({href:"home-assets/uja-boy-thumb.jpg"});
-    }
-    else if (current == 'home2') {
-        jQuery.colorbox({inline:true, href:"#basic-form"});
-    }
-    else if (current == 'home3') {
-        jQuery.colorbox({iframe:true, innerWidth:400, innerHeight:300, href:"http://player.vimeo.com/video/9594822?title=0&amp;byline=0&amp;portrait=0"});
-    }
-    else {
-        //navigation window
-    }
-}
-
-//call each time a direction button is clicked
-function rotControl(direction){
-    var back_class = button_index==0 ? 'home4' : 'home'+(button_index);
-    var back_index = button_index==0 ? 3 : button_index-1;
-    var fore_class = button_index==3 ? 'home1' : 'home'+(button_index+2);
-    var fore_index = button_index==3 ? 0 : button_index+1;
-    var obt = new Object();
-    if(direction.data=='backward'){
-        obt.data = {classto:back_class, button:back_index};    
-    }else{
-        obt.data = {classto:fore_class, button:fore_index};
-    }
-    rotation(obt);
-}
-
-//call each time a button is clicked
-function rotation(cur){
-    jQuery('#bg-container').stop(true, true);//stop the animation
-    clearInterval(rotate_int);//stop the rotate loop
-    var current = jQuery('#bg-container').attr('class');//the current bg-container class
-    var class_to = cur.data.classto;
-    bgClass(current, class_to);
-    
-    var button = cur.data.button;
-    interButton(button);
+function navText(index){
+        jQuery('#nav-box h2').text(jsonAbbrev[index].title);
+        jQuery('#nav-box p:eq(0)').text(jsonAbbrev[index].line1);
+        jQuery('#nav-box p:eq(1)').text(jsonAbbrev[index].line2);
 }
 
 //automatic rotate function of background images
 function rotate() {
-    var current = jQuery('#bg-container').attr('class');//the current bg-container class
-    if (current == 'home1') {
-        bgClass(current, 'home2');
-        interButton(1);
-    }
-    else if (current == 'home2') {
-        bgClass(current, 'home3');
-        interButton(2);
-    }
-    else if (current == 'home3') {
-        bgClass(current, 'home4');
-        interButton(3);
-    }
-    else {
-        bgClass(current, 'home1');
-        interButton(0);
-    }
+        current = jQuery('#bg-container').attr('class');//the current bg-container class
+        classNumber = current.charAt(current.length-1) * 1;
+        newbgclass = classNumber < (totalImages - 1) ? 'home' +  (classNumber + 1) : 'home0';
+        bgClass(current, newbgclass);
+        interButton(classNumber < (totalImages - 1) ? classNumber + 1 : 0);
 };
 
 function bgClass(oldclass, newclass){
-    jQuery('#bg-container').css({ opacity: 0.2 })
-    .removeClass(oldclass)
-    .addClass(newclass)
-    .animate({ opacity: 1.0 }, 2000);
+        jQuery('#bg-container').css({ opacity: 0.2 })
+                .removeClass(oldclass)
+                .addClass(newclass)
+                .animate({ opacity: 1.0 }, fadeTime);
 }
 
 function interButton(linum){
-    button_index = linum;
-    navText(linum);
-    jQuery('#slide-nav li[class*=selected]').removeClass('selected');
-    jQuery('#slide-nav li[class!=navigator]:eq('+linum+')').addClass('selected');
+        button_index = linum;
+        navText(linum);
+        jQuery('#slide-nav li[class*=selected]').removeClass('selected');
+        jQuery('#slide-nav li[class!=navigator]:eq('+linum+')').addClass('selected');
 }
+
+//call each time a direction button is clicked
+function rotControl(direction){
+        maxelement = totalImages - 1;
+        back_class = button_index==0 ? 'home' + maxelement : 'home'+ (button_index-1);
+        back_index = button_index==0 ? maxelement : (button_index-1);
+        fore_class = button_index==maxelement ? 'home0' : 'home'+(button_index+1);
+        fore_index = button_index==maxelement ? 0 : (button_index+1);
+        var obt = new Object();
+        if(direction.data=='backward'){
+           obt.data = {classto:back_class, button:back_index};    
+        }else{
+           obt.data = {classto:fore_class, button:fore_index};
+        }
+        rotation(obt);
+}
+
+//call each time a button is clicked
+function rotation(cur){
+        jQuery('#bg-container').stop(true, true);//stop the animation
+        clearInterval(rotate_int);//stop the rotate loop
+        current = jQuery('#bg-container').attr('class');//the current bg-container class
+        class_to = cur.data.classto;
+        bgClass(current, class_to);
+        
+        button = cur.data.button * 1;
+        interButton(button);
+}
+
+function loadExt() {
+        clearInterval(rotate_int);//stop the rotate loop
+        current = jQuery('#bg-container').attr('class');//the current bg-container class
+        classNumber = current.charAt(current.length-1) * 1;
+        linkpath =jsonAbbrev[classNumber].link.path;
+        if(linkpath.indexOf('iframe') > 0){
+                s0 = linkpath.indexOf('src') + 5;
+                s1 = linkpath.indexOf('"',s0);
+                w0 = linkpath.indexOf('width') + 7;
+                w1 = linkpath.indexOf('"',w0);
+                h0 = linkpath.indexOf('height') + 8;
+                h1 = linkpath.indexOf('"',h0);
+                videowidth = linkpath.slice(w0,w1) * 1;
+                videoheight = linkpath.slice(h0,h1) * 1;
+                videopath = linkpath.slice(s0,s1);
+                videocaption = jsonAbbrev[classNumber].link.optCaption;
+                videolink = jsonAbbrev[classNumber].link.optLink;
+                videotitle = '<p>' + videocaption + '</p>' + '<a href="' + videolink + '" target="_blank" >' + videolink + '</a>';
+                jQuery.colorbox({title:videotitle, iframe:true, innerWidth:videowidth, innerHeight:videoheight, href:videopath});
+        }else{
+                window.open(linkpath, '_blank');
+        }
+        //else if (current == 'home2') {
+        //    jQuery.colorbox({inline:true, href:"#basic-form"});
+        //}
+}
+
