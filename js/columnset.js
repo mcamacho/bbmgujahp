f7bfe6cfd68e8cc92de3ef39e5b375120b22c779
Mauricio Camacho <mcamacho@bbmg.com>
2011-08-01 18:52:35 -0400

    add-box rename and new columnset.js

---
+++ js/columnset.js
@@ -0,0 +1,39 @@
+//js with json list of column data content or parameters
+ 
+var columnset = {
+    'pathFolder':'home-assets/',
+    'column': [
+        {
+            'imgfile': 'uja-boy-thumb.jpg',
+            'contentlink': 'http://www.ujafedny.org',
+            'header': 'Designing Solutions for Families with Autism',
+            'shorttext': 'Sit amet, consectetur adipiscing elit. Fusce scelerisque cursus convallis.',
+            'textlink1': {'link': 'http://www.ujafedny.org', 'text': 'Where Your Donation Goes'},
+            'textlink2': {'link': '', 'text': ''}
+            },
+        {
+            'imgfile': 'uja-boy-thumb.jpg',
+            'contentlink': 'http://www.ujafedny.org',
+            'header': 'Designing Solutions for Families with Autism',
+            'shorttext': 'Sit amet, consectetur adipiscing elit. Fusce scelerisque cursus convallis.',
+            'textlink1': {'link': 'http://www.ujafedny.org/uja-federation-news-2/view/fellowship-brings-young-leaders-to-israel/', 'text': 'Fellowship Brings Young Leaders to Israel'},
+            'textlink2': {'link': 'http://www.ujafedny.org/uja-federation-news-2/view/standing-together-when-it-matters-most/', 'text': 'Standing Together When It Matters Most'}
+            },
+        {
+            'imgfile': 'uja-boy-thumb.jpg',
+            'contentlink': 'http://www.ujafedny.org',
+            'header': 'Designing Solutions for Families with Autism',
+            'shorttext': 'Sit amet, consectetur adipiscing elit. Fusce scelerisque cursus convallis.',
+            'textlink1': {'link': 'http://www.ujafedny.org/get/26697/', 'text': 'Advocate: Check Out Our Public Policy Agenda'},
+            'textlink2': {'link': 'http://www.ujafedny.org/israel-experience/', 'text': 'Experience Israel On A UJA-Federation Mission'}
+            },
+        {
+            'imgfile': 'uja-boy-thumb.jpg',
+            'contentlink': 'http://www.ujafedny.org',
+            'header': 'Designing Solutions for Families with Autism',
+            'shorttext': 'Sit amet, consectetur adipiscing elit. Fusce scelerisque cursus convallis.',
+            'textlink1': {'link': 'http://www.ujafedny.org/the-wall-street-dinner/', 'text': 'Wall Street Dinner'},
+            'textlink2': {'link': 'http://www.ujafedny.org/a-camp-helps-kids-heal/', 'text': 'Hamptons For You. Camp For Kids'}
+            }
+    ]
+};
